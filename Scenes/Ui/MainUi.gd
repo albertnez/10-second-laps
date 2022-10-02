a931e775cd60f2fb1d641c15beb2bf1c980d661b
albertnez <albertmg7@gmail.com>
2022-10-02 18:12:08 +0200

    Volume control now actually works

--- Scenes/Ui/MainUi.gd
+++ Scenes/Ui/MainUi.gd
@@ -1,6 +1,7 @@
 extends Control
 
 onready var _laps_label := $"%LapsLabel"
+onready var _volume_slide := $"%VolumeSlide"
 
 func _ready() -> void:
 	EventBus.connect("laps_changed", self, "_on_EventBus_laps_changed")
@@ -8,3 +9,8 @@ func _ready() -> void:
 
 func _on_EventBus_laps_changed(num_laps: int) -> void:
 	_laps_label.text = str("Laps: ", num_laps)
+
+
+func _on_VolumeSlide_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(0, linear2db(value/_volume_slide.max_value))
+	pass # Replace with function body.
