9de8790aca5cea63374a7202b2c76c40448131f2
albertnez <albertmg7@gmail.com>
2022-10-02 23:47:46 +0200

    better obstacle spawn

--- Scenes/Game/Behaviors/FloatingBehavior.gd
+++ Scenes/Game/Behaviors/FloatingBehavior.gd
@@ -5,4 +5,4 @@ onready var _t := 0.0
 
 func _physics_process(delta: float) -> void:
 	_t += delta
-	_enemy.move_towards_center(100 * sin(TAU * _t * 0.5) * delta)
+	_enemy.move_towards_center(50 * sin(TAU * _t * 0.5) * delta)
--- Scenes/Game/EnemySpawner.gd
+++ Scenes/Game/EnemySpawner.gd
@@ -4,9 +4,11 @@ class_name EnemySpawner
 export (float, 0, 100, 1.0) var MINIMUM_DISTANCE_FROM_CENTER = 0
 
 const MAX_DIST = 185
-const MIN_DIST = 20
+const MIN_DIST = 50
 const packed_enemy := preload("res://Scenes/Game/Enemy.tscn")
 const packed_row_with_floor_gap := preload("res://Scenes/Game/Enemies/RowWithFloorGap.tscn")
+const packed_floating := preload("res://Scenes/Game/Enemies/FloatingEnemy.tscn")
+const packed_big := preload("res://Scenes/Game/Enemies/BigEnemy.tscn")
 
 onready var _timer := $SpawnerTimer
 onready var _needle_rootation := $"%ClockRotation"
@@ -26,6 +28,7 @@ func set_can_spawn(can_spawn: bool) -> void:
 
 func _set_lap(lap: int) -> void:
 	_lap = lap
+	_timer.wait_time = max(0.5, 3.0 - 0.1 * lap)
 
 
 func spawn_enemy(needle: Vector2) -> void:
@@ -34,8 +37,24 @@ func spawn_enemy(needle: Vector2) -> void:
 	add_child(enemy)
 
 
+func _spawn_wavy() -> void:
+	var e : Enemy = packed_floating.instance()
+	
+
 func _on_SpawnerTimer_timeout() -> void:
-	var enemy : Enemy = packed_enemy.instance()
-	var needle := Vector2.UP.rotated(_needle_rootation.rotation - 0.05) * rand_range(MIN_DIST, MAX_DIST)
+	var enemy : Node2D = null
+	var x = randf()
+	var max_distance = MAX_DIST 
+	if x < 0.6:
+		enemy = packed_enemy.instance()
+	elif x < 0.9:
+		enemy = packed_floating.instance()
+	else:
+		enemy = packed_big.instance()
+		max_distance = 170
+	
+	var mult := rand_range(MIN_DIST, max_distance)
+	mult = max_distance
+	var needle := Vector2.UP.rotated(_needle_rootation.rotation - 0.05) * mult
 	enemy.position = needle
 	add_child(enemy)
